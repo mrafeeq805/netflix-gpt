4495757ad731b6a179631066d7b95a7d55b80a60
mrafeeqvr <muhammedrafeeq805@gmail.com>
2024-02-12 14:39:38 +0530

    project initialized using cra

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-lg text-green-700'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
